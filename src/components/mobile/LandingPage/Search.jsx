cba5e62ec903508ebe40b2634453d167c16af78a
tamarazu <tamarazulaika.u@gmail.com>
2024-02-28 19:00:40 +0700

    Menu landing page

--- src/components/mobile/LandingPage/Search.jsx
+++ src/components/mobile/LandingPage/Search.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Search() {
   return (
-    <div>Search</div>
-  )
+    <div className="w-full px-2.5 pb-3">
+      <label className="w-full relative items-center">
+        <input placeholder="Search..." className="border w-full py-1.5 px-3 rounded-md" />
+        <i className="absolute right-3 top-0 fi fi-br-search text-orange-500"></i>
+      </label>
+    </div>
+  );
 }
