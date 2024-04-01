9c130e5298f36eec12d980bd6374c4f2a1ff010b
tamarazu <tamarazulaika.u@gmail.com>
2024-04-01 22:25:34 +0700

    complete search function

--- src/components/mobile/LandingPage/Search.jsx
+++ src/components/mobile/LandingPage/Search.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState , useMemo, useCallback} from "react";
+import { useDispatch } from "react-redux";
+import { getAllMenu, getMenuSearch } from "store/actions/menu";
+import { debounce } from "lodash";
 
 export default function Search() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+    changeFilterDebounced(event.target.value);
+  };
+
+  const changeFilterDebounced = useCallback(debounce(value => {
+    if (value.length) {
+      dispatch(getMenuSearch(value))
+    } else {
+      dispatch(getAllMenu())
+    }
+  }, 500, true), [])
+
   return (
     <div className="w-full px-2.5 pb-3">
       <label className="w-full relative items-center">
-        <input placeholder="Search..." className="border w-full py-1.5 px-3 rounded-md" />
+        <input
+          placeholder="Search..."
+          type="text"
+          className="border w-full py-1.5 px-3 rounded-md"
+          onChange={handleInputChange}
+        />
         <i className="absolute right-3 top-0 fi fi-br-search text-orange-500"></i>
       </label>
     </div>
--- src/store/actions/menu.js
+++ src/store/actions/menu.js
@@ -1,7 +1,12 @@
 import types from "store/types/menu";
 import Axios from "lib/axios";
 import { set_error, set_loading } from "./index";
-import { GET_ALL_MENU, GET_CATEGORIES, GET_MENU_BY_CATEGORY, GET_CATEGORY } from "lib/api";
+import {
+  GET_ALL_MENU,
+  GET_CATEGORIES,
+  GET_MENU_BY_CATEGORY,
+  GET_CATEGORY,
+} from "lib/api";
 
 export const set_all_menu = (value) => {
   return {
@@ -13,16 +18,16 @@ export const set_all_menu = (value) => {
 export const set_categories = (value) => {
   return {
     type: types.SET_CATEGORIES,
-    payload: value
-  }
-}
+    payload: value,
+  };
+};
 
 export const getAllMenu = () => async (dispatch) => {
   try {
     dispatch(set_loading(true));
     const { data, status } = await Axios.get(GET_ALL_MENU);
     if (status === 200) {
-      dispatch(set_all_menu(data));
+      dispatch(set_all_menu(data.data));
     }
   } catch (error) {
     dispatch(set_error("Failed get data"));
@@ -32,14 +37,13 @@ export const getAllMenu = () => async (dispatch) => {
   }
 };
 
-
 export const getMenuByCategory = (value) => async (dispatch) => {
   try {
     dispatch(set_loading(true));
     const { data, status } = await Axios.get(GET_MENU_BY_CATEGORY, {
       params: {
-        category_id: value
-      }
+        category_id: value,
+      },
     });
     if (status === 200) {
       dispatch(set_all_menu(data.data));
@@ -52,8 +56,27 @@ export const getMenuByCategory = (value) => async (dispatch) => {
   }
 };
 
+export const getMenuSearch = (value) => async (dispatch) => {
+  try {
+    dispatch(set_loading(true));
+    const { data, status } = await Axios.get(GET_ALL_MENU + "/search", {
+      params: {
+        name: value,
+      },
+    });
+    if (status === 200) {
+      dispatch(set_all_menu(data.data))
+    }
+  } catch (error) {
+    dispatch(set_error("Failed get data"));
+    dispatch(set_loading(false));
+  } finally {
+    dispatch(set_loading(false));
+  }
+};
+
 export const getCategories = () => async (dispatch) => {
-  dispatch(set_all_menu([]))
+  dispatch(set_all_menu([]));
   try {
     dispatch(set_loading(true));
     const { data, status } = await Axios.get(GET_CATEGORIES);
